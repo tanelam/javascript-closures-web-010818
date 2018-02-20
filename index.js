515ce073b24c35e631b0726a81fd60cd3cad9e38
tanelam <tania.e.aparicio@gmail.com>
2018-02-20 17:42:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 const app = "I don't do much."
+
+function fatBastard(meal){
+  function whatsForDinner(){
+    //whatsForDinner is the inner function, a closure
+    if(!meal) {
+      console.log("My belly is empty. woe is me.");
+    } else if (meal === "Mini-Me"){
+      //whatsForDinner() uses argument provided to the parent
+      //function
+      console.log("The wee man is in my belly!");
+    } else {
+      console.log(`Im eatin a bit of ${meal}!`);
+    }
+  }
+  function digest(){ //digest() is an inner function, a closure
+      meal = undefined;
+  }
+  return { whatsForDinner,
+    digest //we are now returning an object with refereces
+            // to our whatsForDinner() and digest() functions
+  };
+  //we are not excecuting the function, we're returning it
+}
